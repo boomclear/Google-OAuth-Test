f22f42abf689ac7e89c9424abfa2d7b25f46ab22
Austin Woods <austinwoods99@gmail.com>
2023-08-02 09:30:31 -0400

    Add event new form, migration, and event index

---
+++ db/migrate/20230802132258_create_event.rb
@@ -0,0 +1,11 @@
+class CreateEvent < ActiveRecord::Migration[7.0]
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.string :description
+      t.datetime :date_time
+
+      t.timestamps
+    end
+  end
+end
